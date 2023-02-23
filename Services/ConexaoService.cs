68e9398ab5e199cb91847f26db94e494222fd67d
Victor José Costa Farias <47372055+Petrucchio@users.noreply.github.com>
2023-02-23 20:18:49 -0300

    Update

--- Services/ConexaoService.cs
+++ Services/ConexaoService.cs
@@ -11,17 +11,16 @@ namespace Exercicio_alfa_people.Services
     {
         public static CrmServiceClient GetService()
         {
-            string url = "";
-            string clientId = "";
-            string clientSecret = "";
+            string url = "https://orgbf9820cf.crm2.dynamics.com/";
+            string clientId = "f8d1b742-37df-4cd1-9deb-2efd717664ba";
+            string clientSecret = "flf8Q~RAbjrc3U3oCf4EyUxeAulB5.bfEyAavcrL";
             CrmServiceClient serviceClient = new CrmServiceClient($"AuthType=ClientSecret;Url={url};AppId={clientId};ClientSecret={clientSecret};");
-
             if (!serviceClient.CurrentAccessToken.Equals(null))
                 Console.WriteLine("Conexão Realizada com Sucesso");
             else
                 Console.WriteLine("Erro na conexão");
-                return serviceClient;
-            
+            return serviceClient;
+
         }
     }
 }
